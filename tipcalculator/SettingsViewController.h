2d31be833696c9db21156d350e75f9957739230c
Bharti Agrawal <bharti@yahoo-inc.com>
2014-06-05 19:12:20 -0700

    initial commit

---
+++ tipcalculator/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  tipcalculator
+//
+//  Created by Bharti Agrawal on 6/4/14.
+//  Copyright (c) 2014 Yahoo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Bharti Agrawal on 6/4/14.
+//  Copyright (c) 2014 Yahoo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
